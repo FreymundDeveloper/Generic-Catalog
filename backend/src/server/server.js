59d64b262820d59d36fc9a333ae7f45ef63425c6
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-01-09 22:40:24 -0300

    Backend Initial Logic

---
+++ backend/src/server/server.js
@@ -0,0 +1,57 @@
+// server.js
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const data = require(path.join(__dirname, '..', 'data', 'data.json'));
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+app.get('/products', (req, res) => {
+  const { types, sellers, sports, availableSizes, priceRange } = req.query;
+
+  let filteredProducts = data;
+
+  console.log('Received types:', types);
+
+  if (types !== undefined) {
+    const selectedTypes = Array.isArray(types) ? types : types.split(',');
+    console.log(selectedTypes)
+
+    filteredProducts = filteredProducts.filter(product => selectedTypes.includes(product.type));
+  }
+
+  if (sellers !== undefined) {
+    const selectedSellers = Array.isArray(sellers) ? sellers : sellers.split(',');
+
+    filteredProducts = filteredProducts.filter(product => selectedSellers.includes(product.seller));
+  }
+
+  if (sports !== undefined) {
+    const selectedSports = Array.isArray(sports) ? sports : sports.split(',');
+
+    filteredProducts = filteredProducts.filter(product => selectedSports.includes(product.sport));
+  }
+
+  if (availableSizes !== undefined) {
+    const selectedSizes = Array.isArray(availableSizes) ? availableSizes : availableSizes.split(',');
+
+    filteredProducts = filteredProducts.filter(product => {
+      return product.available_sizes.some(size => selectedSizes.includes(size));
+    });
+  }
+
+  if (priceRange !== undefined) {
+    const maxPrice = parseFloat(priceRange);
+
+    if (!isNaN(maxPrice)) filteredProducts = filteredProducts.filter(product => product.price <= maxPrice);
+  }
+
+  res.json(filteredProducts);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
