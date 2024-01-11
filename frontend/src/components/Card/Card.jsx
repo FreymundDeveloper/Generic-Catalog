0c442dbc05750443414dce220aec63898eb33a5a
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-01-11 06:43:30 -0300

    Card Component Improvement

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { Modal } from '../../components';
 
 export const Card = ({ cardData }) => {
-    const { image_url, name, price, details, seller } = cardData;
+    const { available_sizes, image_url, name, price, details, seller } = cardData;
     const [modalOpen, setModalOpen] = useState(false);
 
     const openModal = () => {
@@ -20,7 +20,12 @@ export const Card = ({ cardData }) => {
             <CardImage src={image_url} alt={name} />
             <CardContent>
                 <CardName>{name}</CardName>
-                <CardPrice>R$ {price}</CardPrice>
+                <PriceContainer>
+                    {available_sizes.length === 0 && (
+                          <SoldOutText>Sold Off</SoldOutText>
+                    )}
+                    <CardPrice>R$ {price}</CardPrice>
+                </PriceContainer>
             </CardContent>
         </CardContainer>
         <Modal isOpen={modalOpen} description={details} seller={seller} onClose={closeModal} />
@@ -35,39 +40,51 @@ const Container = styled.div`
 `;
 
 const CardContainer = styled.div`
-  border: 1px solid #ddd;
-  border-radius: 8px;
-  overflow: hidden;
-  width: 200px;
-  margin: 16px;
-  border-radius: 12px;
-  background-color: ${(props) => props.theme.color.cardColor};
-  cursor: pointer;
+    border: 1px solid #ddd;
+    border-radius: 8px;
+    overflow: hidden;
+    width: 200px;
+    margin: 16px;
+    border-radius: 12px;
+    background-color: ${(props) => props.theme.color.cardColor};
+    cursor: pointer;
 `;
 
 const CardImage = styled.img`
-  width: 180px;
-  height: 150px;
-  object-fit: cover;
-  margin-top: 10px;
-  border: 1px solid #ddd;
-  border-radius: 12px;
+    width: 180px;
+    height: 150px;
+    object-fit: cover;
+    margin-top: 10px;
+    border: 1px solid #ddd;
+    border-radius: 12px;
 `;
 
 const CardContent = styled.div`
-  padding: 12px;
+    padding: 12px;
 `;
 
 const CardName = styled.h3`
-  font-size: 16px;
-  height: 42px;
-  margin: 8px 0;
-  text-align: left;
+    font-size: 16px;
+    height: 42px;
+    margin: 8px 0;
+    text-align: left;
+`;
+const PriceContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 `;
 
 const CardPrice = styled.p`
-  font-size: 14px;
-  font-weight: bold;
-  color: #06b600;
-  text-align: right;
+    font-size: 14px;
+    font-weight: bold;
+    color: #06b600;
+    text-align: right;
+`;
+
+const SoldOutText = styled.p`
+    font-size: 12px;
+    font-weight: bold;
+    color: red;
+    text-align: left;
 `;
