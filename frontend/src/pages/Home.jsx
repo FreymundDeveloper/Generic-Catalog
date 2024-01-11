b89ffd7689b73acc348bdb525362ea1ed8f4e2ed
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-01-11 09:47:08 -0300

    Home Adjustment

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -24,7 +24,6 @@ export const Home = () => {
             });
     }, [sidebarFilter]);
     
-
     const handleSidebarFilterChange = (filterContent, name) => {
         setSidebarFilter(queryParamsBuilder(filterContent, name));
     };
