ccc736f131db5b5faece5af098911bc278e486fc
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-01-11 09:06:35 -0300

    Alert to ButtonNavbar

--- frontend/src/components/Button/ButtonNavbar.jsx
+++ frontend/src/components/Button/ButtonNavbar.jsx
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { darken } from 'polished';
 
-export const ButtonNavbar = ({ children, onClick }) => {
-    return (
-        <ButtonStyled onClick={onClick}>{children}</ButtonStyled>
-    );
+export const ButtonNavbar = ({ children }) => {
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleButtonClick = () => { // Since the button does nothing, call an alert!
+    setShowAlert(true);
+
+    setTimeout(() => {
+        setShowAlert(false);
+    }, 1000);
+  };
+
+  return (
+    <>
+      <ButtonStyled onClick={handleButtonClick}>{children}</ButtonStyled>
+      {showAlert && (
+        <AlertMessage>
+          Under Maintenance!
+        </AlertMessage>
+      )}
+    </>
+  );
 };
 
 const ButtonStyled = styled.button`
-    background-color: ${(props) => props.theme.color.navbarColor};
-    color: white;
-    border: none;
-    padding: 8px 12px;
-    cursor: pointer;
+  background-color: ${(props) => props.theme.color.navbarColor};
+  color: white;
+  border: none;
+  padding: 8px 12px;
+  cursor: pointer;
 
-    &:hover {
-        background-color: ${(props) => darken(0.1, props.theme.color.navbarColor)};
-    }
+  &:hover {
+    background-color: ${(props) => darken(0.1, props.theme.color.navbarColor)};
+  }
+`;
+
+const AlertMessage = styled.div`
+  position: fixed;
+  font-size: 14px;
+  top: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: ${(props) => props.theme.color.checkboxColor};
+  color: white;
+  padding: 8px 12px;
+  border-radius: 4px;
+  z-index: 1000;
 `;
